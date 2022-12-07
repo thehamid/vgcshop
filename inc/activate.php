8b4552d30d41ea6ed291942d6dff6a88b16dce6d
Hamid Amini <info@thehamid.ir>
2022-12-07 08:21:59 +0330

    sql query for create table orders

--- inc/activate.php
+++ inc/activate.php
@@ -13,9 +13,20 @@ if ( ! function_exists( 'vgc_activate_plugin' ) ){
         $table_name = $wpdb->prefix . 'vgcshop_orders';
         $charset_collate = $wpdb->get_charset_collate();
 
-        $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-        id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        email_id varchar(50) NOT NULL default '') $charset_collate;";
+        $sql = "CREATE TABLE IF NOT EXISTS $table_name  ( 
+        `id` BIGINT(20) NOT NULL AUTO_INCREMENT ,
+        `price_card` INT(11) NOT NULL ,
+         `receiver_name` VARCHAR(255) NULL ,
+          `receiver_phone` INT(11) NOT NULL ,
+           `message` TEXT NULL ,
+            `sender_name` VARCHAR(255) NULL ,
+             `sender_phone` INT(11) NOT NULL ,
+              `card_id` INT(11) NOT NULL ,
+               `code` VARCHAR(11) NOT NULL ,
+                `trans_id` LONGTEXT NOT NULL 
+                 `alert` TEXT NULL ,
+                  `date_order` DATETIME NOT NULL ,
+                   `status` INT NOT NULL , PRIMARY KEY (`id`)) $charset_collate;";
 
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
         dbDelta( $sql );
